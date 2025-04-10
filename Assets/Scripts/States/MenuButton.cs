1d3a1e7fbd3b01725a38d9972883b3a5d2accb07
Mahmoud <mahmoud.sayed235@gmail.com>
2025-04-10 22:00:52 +0200

    Update MenuButton.cs

--- Assets/Scripts/States/MenuButton.cs
+++ Assets/Scripts/States/MenuButton.cs
@@ -2,7 +2,8 @@ using UnityEngine;
 
 public class MenuButton : MonoBehaviour
 {
-    [SerializeField] private string videoName;
+    [SerializeField] 
+    private string videoName;
     public void OnClick()
     {
         AppStateManager.Instance.SetState(new PlaybackState(videoName));
